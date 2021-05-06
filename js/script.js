9c5a43b228847bd5602a76801f04a0c8d2c2701b
fajarfadillahh <fajaragustian979@gmail.com>
2021-05-06 11:33:59 +0700

    add scroll animation

--- js/script.js
+++ js/script.js
@@ -22,3 +22,34 @@ function linkAction() {
   navMenu.classList.remove("show-menu");
 }
 navLink.forEach((e) => e.addEventListener("click", linkAction));
+
+// SCROLL REVEAL ANIMATION
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: "2000",
+  reset: true,
+});
+
+// SCROLL ANIMATION HOME
+sr.reveal(".home__title");
+sr.reveal(".home__button", { delay: 200 });
+sr.reveal(".home__img", { delay: 400 });
+sr.reveal(".home__social-icon", { interval: 200 });
+
+// SCROLL ANIMATION ABOUT
+sr.reveal(".about__img");
+sr.reveal(".about__subtitle", { delay: 200 });
+sr.reveal(".about__text", { delay: 400 });
+
+// SCROLL ANIMATION SKILLS
+sr.reveal(".skills__subtitle");
+sr.reveal(".skills__text", { delay: 200 });
+sr.reveal(".skills__data", { delay: 400 });
+sr.reveal(".skills__img", { delay: 600 });
+
+// SCROLL ANIMATION WORK
+sr.reveal(".work__img", { interval: 200 });
+
+// SCROLL ANIMATION CONTACT
+sr.reveal(".contact__input", { interval: 200 });
